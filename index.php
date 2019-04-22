1d0354454ede3296aa3896abbd80f0a739885ba4
Junjie <dshen123456@gmail.com>
2019-04-22 22:52:47 +0200

    1

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>Document</title>
+   <title>Iniciar Sesión</title>
+   <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
       <?php
@@ -17,13 +18,15 @@
       /* DESTRUCCIÓN DE LA SESIÓN */
       session_destroy();
       ?>
-   <div>
-      <p>Usuario:</p>
+   <div id="cajaUsu">
+         <p>Usuario:</p>
       <input type="text" id="usuario">
       <p>Clave:</p>
-      <input type="text" id="clave">
+      <input type="text" id="clave"><br>
       <input type="button" id="inisesion" value="iniciar sesión">
       <div id="resultado">&nbsp;</div>
+      <p>Usuario: user<br>Clave: user</p><br>
+      <p>Usuario: admin<br>Clave: admin</p><br>
    </div>
   
 </body>
